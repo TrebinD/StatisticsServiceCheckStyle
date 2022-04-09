97f310bc553ab79aff2d6c2e1efe300d455830b9
Dmitrii Trebin <d.trebin@rambler.ru>
2022-04-09 21:33:56 +0300

    add checkstyle

---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticsServiceTest {
+    StatisticsService service = new StatisticsService();
+
+
+    @Test
+    void findMax() {
+
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void cuurentMax() {
+
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 12, 11, 14};
+        long expected = 14;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+}
